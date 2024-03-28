623b4aa7e2f58950a3a34313077ca442be7ef9f4
suanzyy <138212080+suanzyy@users.noreply.github.com>
2024-03-28 13:41:09 +0000

    Add files via upload

---
+++ game_data.py
@@ -0,0 +1,50 @@
+level_0 = {
+    'terrain' : r"D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._terrain.csv",
+    'coins' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._coins.csv',
+    'fg palms' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._fg_palms.csv',
+    'bg palms': r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._bg_palms.csv',
+    'crates' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._crates.csv',
+    'enemies' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._enemies.csv',
+    'constraints':r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._constraints.csv',
+    'player': r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._player.csv',
+    'grass' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\0\untitled._grass.csv',
+    'node_pos' : (110,400),
+    'unlock' : 1,
+    'node_graphics' : r"D:\COMPUTER SCIENCE NEA 2024\tiled\graphics\overworld\0"
+}
+
+level_1 = {
+    'terrain' : r"D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_terrain.csv",
+    'coins' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_coins.csv',
+    'fg palms' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_fg_palms.csv',
+    'bg palms': r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_bg_palms.csv',
+    'crates' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_crates.csv',
+    'enemies' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_enemies.csv',
+    'constraints':r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_constraints.csv',
+    'player': r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_player.csv',
+    'grass' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\1\level_1_grass.csv',
+    'node_pos' : (480,610),
+    'unlock' : 2,
+    'node_graphics' : r"D:\COMPUTER SCIENCE NEA 2024\tiled\graphics\overworld\1"
+}
+
+level_2 = {
+    'terrain' : r"D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_terrain.csv",
+    'coins' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_coins.csv',
+    'fg palms' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_fg_palms.csv',
+    'bg palms': r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_bg_palms.csv',
+    'crates' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_crates.csv',
+    'enemies' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_constraints.csv',
+    'constraints':r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_terrain.csv',
+    'player': r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_player.csv',
+    'grass' : r'D:\COMPUTER SCIENCE NEA 2024\tiled\levels\2\level_2_grass.csv',
+    'node_pos' : (1050,400),
+    'unlock' : 2,
+    'node_graphics' : r"D:\COMPUTER SCIENCE NEA 2024\tiled\graphics\overworld\2"
+}
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2
+}
